bc4dab9d8787776cf9603a724c71bea29418f217
Brian Elizondo <brian_elizondo@msn.com>
2022-09-09 19:58:08 -0400

    Data Structures => Divide and Conquer problem solving: countZeroes, Given an array of 1s and 0s which has all 1s first followed by all 0s, write a function called countZeroes, which returns the number of zeroes in the array

--- count-zeroes.js
+++ count-zeroes.js
@@ -1,5 +1,25 @@
-function countZeroes() {
-  
+function countZeroes(arr) {
+    let val = 0;
+    let leftIdx = 0;
+    let rightIdx = arr.length - 1;
+
+    while (leftIdx <= rightIdx) {
+        let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+        let middleVal = arr[middleIdx];
+    
+        if (middleVal > val){
+            leftIdx = middleIdx + 1;
+        } else if(arr[leftIdx] > val){
+            leftIdx++;
+        } else if(arr[leftIdx] == val){
+            return arr.length - leftIdx;
+        } else if(middleVal == val){
+            return arr.length - middleIdx;
+        }
+    }
+
+    return 0;
 }
 
-module.exports = countZeroes
+module.exports = countZeroes;
+
