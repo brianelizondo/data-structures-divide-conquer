519135387340a2774a5c1352163f410368a1defc
Brian Elizondo <brian_elizondo@msn.com>
2022-09-09 20:48:44 -0400

    Data Structures => Divide and Conquer problem solving: sortedFrequency, Given a sorted array and a number, write a function called sortedFrequency that counts the occurrences of the number in the array

--- sorted-frequency.js
+++ sorted-frequency.js
@@ -1,5 +1,22 @@
-function sortedFrequency() {
+function sortedFrequency(arr, val) {
+    let leftIdx = 0;
+    let rightIdx = arr.length - 1;
 
+    while (leftIdx <= rightIdx) {
+        let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+        let middleVal = arr[middleIdx];
+
+        if(arr[leftIdx] < val){
+            leftIdx++;
+        } else if(arr[rightIdx] > val){
+            rightIdx--;
+        } else if(arr[leftIdx] == val && arr[rightIdx] == val){
+            return (rightIdx - leftIdx) + 1;
+        }
+    }
+
+    return -1;
 }
 
-module.exports = sortedFrequency
+module.exports = sortedFrequency;
+
