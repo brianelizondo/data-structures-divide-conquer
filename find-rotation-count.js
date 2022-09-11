d31c7019c7a584cda12fc76a2f9c36b424a8402c
Brian Elizondo <brian_elizondo@msn.com>
2022-09-11 09:31:28 -0400

    Data Structures => Divide and Conquer problem solving: findRotationCount, Write a function called findRotationCount which accepts an array of distinct numbers sorted in increasing order. The array has been rotated counter-clockwise n number of times. Given such an array, find the value of n

--- find-rotation-count.js
+++ find-rotation-count.js
@@ -1,5 +1,21 @@
-function findRotationCount() {
-  
+function findRotationCount(arr) {
+    let leftIdx = 0;
+    let rightIdx = arr.length - 1;
+
+    while (leftIdx <= rightIdx) {
+        let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+        let middleVal = arr[middleIdx];
+
+        if(middleVal > arr[middleIdx + 1]){
+            return middleIdx + 1;
+        }else if(arr[leftIdx] <= middleVal){
+            leftIdx = middleIdx + 1;
+        }else{
+            rightIdx = middleIdx - 1;
+        }
+    }
+
+    return 0;
 }
 
 module.exports = findRotationCount
