8152342498e8003f7b2fc852b644094ad94a0b15
Brian Elizondo <brian_elizondo@msn.com>
2022-09-11 09:28:20 -0400

    Data Structures => Divide and Conquer problem solving: findRotatedIndex, Write a function called findRotatedIndex which accepts a rotated array of sorted numbers and an integer. The function should return the index of num in the array. If the value is not found, return -1

--- find-rotated-index.js
+++ find-rotated-index.js
@@ -1,5 +1,45 @@
-function findRotatedIndex() {
- 
+function checkPivot(arr){
+    let leftIdx = 0;
+    let rightIdx = arr.length - 1;
+
+    while (leftIdx <= rightIdx) {
+        let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+        let middleVal = arr[middleIdx];
+
+        if(middleVal > arr[middleIdx + 1]){
+            return middleIdx + 1;
+        }else if(arr[leftIdx] <= middleVal){
+            leftIdx = middleIdx + 1;
+        }else{
+            rightIdx = middleIdx - 1;
+        }
+    }
 }
 
-module.exports = findRotatedIndex
+function findValue(arr, val, leftIdx, rightIdx){
+    while (leftIdx <= rightIdx) {
+        let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+        let middleVal = arr[middleIdx];
+
+        if (middleVal < val) {
+            leftIdx = middleIdx + 1;
+        } else if (middleVal > val) {
+            rightIdx = middleIdx - 1;
+        } else {
+            return middleIdx;
+        }
+    }
+
+    return -1;
+}
+
+function findRotatedIndex(arr, val) {
+    let pivot = checkPivot(arr);
+    if(val >= arr[0] && val <= arr[pivot - 1]){
+        return findValue(arr, val, 0, pivot - 1);
+    }else{
+        return findValue(arr, val, pivot, arr.length - 1);
+    }
+}
+
+module.exports = findRotatedIndex;
