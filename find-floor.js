32e2f8ead74f1fecc3c8ffd9a998f4e2e7654b7a
Brian Elizondo <brian_elizondo@msn.com>
2022-09-11 10:10:06 -0400

    Data Structures => Divide and Conquer problem solving: findFloor, Write a function called findFloor which accepts a sorted array and a value x, and returns the floor of x in the array. The floor of x in an array is the largest element in the array which is smaller than or equal to x. If the floor does not exist, return -1

--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,23 @@
-function findFloor() {
-  
+function findFloor(arr, val){
+    let leftIdx = 0;
+    let rightIdx = arr.length - 1;
+
+    while (leftIdx <= rightIdx) {
+        let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+        let middleVal = arr[middleIdx];
+
+        if ((arr[leftIdx] < val && arr[rightIdx] < val)) {
+            return arr[rightIdx];
+        } else if(leftIdx == middleIdx && rightIdx == middleIdx && middleVal > val && middleIdx > 0){
+            return arr[middleIdx - 1];
+        }else if (middleVal > val) {
+            rightIdx = middleIdx - 1;
+        } else if (middleVal < val) {
+            leftIdx = middleIdx + 1;
+        }
+    }
+
+    return -1;
 }
 
-module.exports = findFloor
+module.exports = findFloor;
